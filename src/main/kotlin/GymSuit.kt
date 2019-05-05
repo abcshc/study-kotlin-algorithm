3bceda85168dd78b94df4ff18dbec101f501a4b7
소희철 <thrrkthrrk@gmail.com>
2019-05-06 07:51:39 +0900

    Add test case in GymSuit

--- src/main/kotlin/GymSuit.kt
+++ src/main/kotlin/GymSuit.kt
@@ -1,6 +1,8 @@
 class GymSuit {
-    fun solution(n: Int, lost: IntArray, reserve: IntArray): Int {
-        var answer = 0
-        return answer
+    companion object {
+        fun solution(n: Int, lost: IntArray, reserve: IntArray): Int {
+            var answer = 0
+            return answer
+        }
     }
 }
--- src/test/kotlin/GymSuitTest.kt
+++ src/test/kotlin/GymSuitTest.kt
@@ -1,8 +1,38 @@
 import org.junit.Test
+import kotlin.test.assertEquals
 
-class GymSuitTest{
+class GymSuitTest {
     @Test
-    fun test(){
+    fun `기본 결과값`() {
+        assertEquals(
+            5,
+            GymSuit.solution(5, intArrayOf(2, 4), intArrayOf(1, 3, 5))
+        )
+
+        assertEquals(
+            4,
+            GymSuit.solution(5, intArrayOf(2, 4), intArrayOf(3))
+        )
+        assertEquals(
+            2,
+            GymSuit.solution(3, intArrayOf(3), intArrayOf(1))
+        )
+    }
+
+    @Test
+    fun `최소값 2명`() {
+        assertEquals(
+            2,
+            GymSuit.solution(2, intArrayOf(1), intArrayOf(2))
+        )
+    }
+
+    @Test
+    fun `최대값 30명`() {
+        assertEquals(
+            27,
+            GymSuit.solution(30, intArrayOf(1, 2, 3, 4), intArrayOf(5))
+        )
 
     }
 }
