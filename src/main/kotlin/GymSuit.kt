638fa73f2b92f8edb0c53554167fa0015512418b
소희철 <thrrkthrrk@gmail.com>
2019-05-06 11:38:59 +0900

    Implement GymSuit but not pass all tests

--- src/main/kotlin/GymSuit.kt
+++ src/main/kotlin/GymSuit.kt
@@ -1,8 +1,40 @@
 class GymSuit {
     companion object {
         fun solution(n: Int, lost: IntArray, reserve: IntArray): Int {
-            var answer = 0
-            return answer
+            var reserveIndex = 0
+            var unhappyFriends = 0
+            var sortedReserve = reserve.sortedArray()
+            var lender = ArrayList<Int>()
+            lost.sortedArray().forEach {
+                if (reserveIndex == sortedReserve.size)
+                    unhappyFriends++
+                reserve@ for (i in reserveIndex until sortedReserve.size) {
+                    if (lender.contains(it)) {
+                        unhappyFriends++
+                        break@reserve
+                    }
+                    if (sortedReserve[reserveIndex] == it){
+                        reserveIndex++
+                        break@reserve
+                    }
+                    if (sortedReserve[i] < it - 1)
+                        reserveIndex++
+                    if (sortedReserve[i] == it - 1) {
+                        reserveIndex++
+                        break@reserve
+                    }
+                    if (sortedReserve[i] == it + 1) {
+                        lender.add(sortedReserve[i])
+                        reserveIndex++
+                        break@reserve
+                    }
+                    if (sortedReserve[i] > it + 1 || i == reserve.size - 1) {
+                        unhappyFriends++
+                        break@reserve
+                    }
+                }
+            }
+            return n - unhappyFriends
         }
     }
 }
--- src/test/kotlin/GymSuitTest.kt
+++ src/test/kotlin/GymSuitTest.kt
@@ -3,16 +3,23 @@ import kotlin.test.assertEquals
 
 class GymSuitTest {
     @Test
-    fun `기본 결과값`() {
+    fun `기본 결과값1`() {
         assertEquals(
             5,
             GymSuit.solution(5, intArrayOf(2, 4), intArrayOf(1, 3, 5))
         )
+    }
 
+    @Test
+    fun `기본 결과값2`() {
         assertEquals(
             4,
             GymSuit.solution(5, intArrayOf(2, 4), intArrayOf(3))
         )
+    }
+
+    @Test
+    fun `기본 결과값3`() {
         assertEquals(
             2,
             GymSuit.solution(3, intArrayOf(3), intArrayOf(1))
@@ -34,5 +41,85 @@ class GymSuitTest {
             GymSuit.solution(30, intArrayOf(1, 2, 3, 4), intArrayOf(5))
         )
 
+    }
+
+    @Test
+    fun `1명만 여벌이 있는 경우`() {
+        assertEquals(
+            2,
+            GymSuit.solution(6, intArrayOf(1, 2, 3, 4, 5), intArrayOf(6))
+        )
+    }
+
+    @Test
+    fun `여별을 가져왔지만 도난당한 경우1`() {
+        assertEquals(
+            2,
+            GymSuit.solution(6, intArrayOf(1, 2, 3, 4, 5, 6), intArrayOf(4, 5))
+        )
+    }
+
+    @Test
+    fun `여별을 가져왔지만 도난당한 경우2`() {
+        assertEquals(
+            1,
+            GymSuit.solution(6, intArrayOf(1, 2, 3, 4, 5, 6), intArrayOf(6))
+        )
+    }
+
+    @Test
+    fun `여별을 가져왔지만 도난당한 경우3`() {
+        assertEquals(
+            5,
+            GymSuit.solution(5, intArrayOf(1, 2, 3, 4, 5), intArrayOf(1, 2, 3, 4, 5))
+        )
+    }
+
+    @Test
+    fun `여별을 가져왔지만 도난당한 경우4`() {
+        assertEquals(
+            5,
+            GymSuit.solution(5, intArrayOf(1), intArrayOf(1, 4, 5))
+        )
+    }
+
+    @Test
+    fun `여별을 가져왔지만 도난당한 경우5`() {
+        assertEquals(
+            4,
+            GymSuit.solution(5, intArrayOf(1, 2), intArrayOf(1, 4, 5))
+        )
+    }
+
+    @Test
+    fun `여별을 가져왔지만 도난당한 경우6`() {
+        assertEquals(
+            4,
+            GymSuit.solution(5, intArrayOf(1, 2, 3), intArrayOf(1, 3, 4, 5))
+        )
+    }
+
+    @Test
+    fun `여별을 가져왔지만 도난당한 경우7`() {
+        assertEquals(
+            2,
+            GymSuit.solution(4, intArrayOf(1, 2, 3, 4), intArrayOf(2, 4))
+        )
+    }
+
+    @Test
+    fun `여별을 가져왔지만 도난당한 경우8`() {
+        assertEquals(
+            3,
+            GymSuit.solution(5, intArrayOf(1, 2, 4, 5), intArrayOf(2, 5))
+        )
+    }
+
+    @Test
+    fun `여별을 가져왔지만 도난당한 경우9`() {
+        assertEquals(
+            4,
+            GymSuit.solution(5, intArrayOf(4, 5), intArrayOf(5))
+        )
     }
 }
